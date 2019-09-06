8f24585c1bf22f24d35c276a213506d395a503c8
JanaSundar <janasundar1997@gmail.com>
2019-09-06 07:51:47 +0000

    Updates

---
+++ precache-manifest.2a8a9f6e1efafbfc85044df5cd2aaf6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92c22d5dc00a12ef883b0906af441ed",
+    "url": "/markdown_editor/index.html"
+  },
+  {
+    "revision": "74f94d6d3c6947d029e2",
+    "url": "/markdown_editor/static/css/main.4b8a7ec4.chunk.css"
+  },
+  {
+    "revision": "efe70ef6943a7c9ce12d",
+    "url": "/markdown_editor/static/js/2.56497267.chunk.js"
+  },
+  {
+    "revision": "74f94d6d3c6947d029e2",
+    "url": "/markdown_editor/static/js/main.4111ee72.chunk.js"
+  },
+  {
+    "revision": "20aa4c3583526156e261",
+    "url": "/markdown_editor/static/js/runtime~main.48126919.js"
+  }
+]);
